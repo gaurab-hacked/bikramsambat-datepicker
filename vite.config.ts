b9bbeabd806f1dd5ae2961a1dcf981607399352e
gaurab-hacked <gaurabsunar0001@gmail.com>
2025-06-21 13:30:22 +0545

    before .css build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import libCss from "vite-plugin-libcss";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), , tailwindcss(), libCss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
